d1942b7bf9123228485bcf4e657e8a1ff4326fbe
ilmiDs <ilmiibokcom@gmail.com>
2022-06-25 00:43:37 +0800

    t

--- backend/repository/Buku.go
+++ backend/repository/Buku.go
@@ -4,99 +4,70 @@ import (
 	"database/sql"
 	"go/types"
 
-	"github.com/ilmiDs/final-project-78-km/backend/models"
+	"github.com/ilmiDs/final-project-78-km/models"
+	"github.com/ilmiDs/final-project-78-km/db"
 )
 
-type BukuRepository interface {
-	GetAll() ([]models.Buku, error)
-	GetById(id int) (models.Buku, error)
-	CreateBuke(buku models.Buku) error
-	UpdateBuku(buku models.Buku) error
-	DeleteBuku(id int) error
-	SearchBuku(search string) ([]models.Buku, error)
-}
-
-	types buku struct {
+	type buku struct {
 		db *sql.DB
 	}
 
-	func NewBukuRepository (db *sql.DB) BukuRepository {
-		return &buku{db}
+	func NewBukuRepository (db *sql.DB) buku {
+		return buku{db}
 	}
 
-	func (buku *buku) GetAll() ([]models.Buku, error) {
-		var bukus []models.Buku
-		rows, err := buku.db.Query("SELECT * FROM buku")
-		if err != nil {
-			return nil, err
-		}
-		defer rows.Close()
-		for rows.Next() {
-			var buku models.Buku
-			err := rows.Scan(&buku.ID, &buku.ISBN, &buku.Judul, &buku.Pengarang, &buku.Penerbit, &buku.Tahun, &buku.Stok, &buku.KotaTerbit, &buku.Deskripsi, &buku.Gambar)
-			if err != nil {
-				return nil, err
-			}
-			bukus = append(bukus, buku)
-		}
-		return bukus, nil
-	}
-
-	func (buku *buku) GetById(id int) (models.Buku, error) {
+	func (b buku) LoadBuku (id int) models.Buku {
 		var buku models.Buku
-		err := buku.db.QueryRow("SELECT * FROM buku WHERE id=?", id).Scan(&buku.ID, &buku.ISBN, &buku.Judul, &buku.Pengarang, &buku.Penerbit, &buku.Tahun, &buku.Stok, &buku.KotaTerbit, &buku.Deskripsi, &buku.Gambar)
-		if err != nil {
-			return buku, err
-		}
-		return buku, nil
+		buku.Id = id
+		return buku
 	}
 
-	// create buku
-	func (buku *buku) CreateBuke(buku models.Buku) error {
-		query := "INSERT INTO buku (isbn, judul, pengarang, penerbit, tahun, stok, kotaTerbit, deskripsi, gambar) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
-		_, err := buku.db.Exec(query, buku.ISBN, buku.Judul, buku.Pengarang, buku.Penerbit, buku.Tahun, buku.Stok, buku.KotaTerbit, buku.Deskripsi, buku.Gambar)
-		if err != nil {
-			return err
-		}
-		return nil
-	}
-
-	// update buku
-	func (buku *buku) UpdateBuku(buku models.Buku) error {
-		query := "UPDATE buku SET isbn=?, judul=?, pengarang=?, penerbit=?, tahun=?, stok=?, kotaTerbit=?, deskripsi=?, gambar=? WHERE id=?"
-		_, err := buku.db.Exec(query, buku.ISBN, buku.Judul, buku.Pengarang, buku.Penerbit, buku.Tahun, buku.Stok, buku.KotaTerbit, buku.Deskripsi, buku.Gambar, buku.ID)
-		if err != nil {
-			return err
-		}
-		return nil
-	}
-
-	// delete buku
-	func (buku *buku) DeleteBuku(id int) error {
-		query := "DELETE FROM buku WHERE id=?"
-		_, err := buku.db.Exec(query, id)
-		if err != nil {
-			return err
-		}
-		return nil
-	}
-
-	// search buku
-	func (buku *buku) SearchBuku(search string) ([]models.Buku, error) {
+	func (b buku) LoadAllBuku () []models.Buku {
 		var bukus []models.Buku
-		query := "SELECT * FROM buku WHERE judul LIKE ? OR pengarang LIKE ? OR penerbit LIKE ? OR tahun LIKE ? OR kotaTerbit LIKE ? OR deskripsi LIKE ? OR gambar LIKE ?"
-		rows, err := buku.db.Query(query, "%"+search+"%", "%"+search+"%", "%"+search+"%", "%"+search+"%", "%"+search+"%", "%"+search+"%", "%"+search+"%")
-		if err != nil {
-			return nil, err
-		}
-		defer rows.Close()
-		for rows.Next() {
-			var buku models.Buku
-			err := rows.Scan(&buku.ID, &buku.ISBN, &buku.Judul, &buku.Pengarang, &buku.Penerbit, &buku.Tahun, &buku.Stok, &buku.KotaTerbit, &buku.Deskripsi, &buku.Gambar)
-			if err != nil {
-				return nil, err
-			}
-			bukus = append(bukus, buku)
-		}
-		return bukus, nil
+		db.Db.Select(&bukus, "SELECT * FROM buku")
+		return bukus
 	}
+
+	//create buku
+	func (b buku) CreateBuku (id int, judul string, penerbit string, pengarang string, tahun string, stok string, harga string, gambar string) models.Buku {
+		var buku models.Buku
+		buku.Id = id
+		buku.Judul = judul
+		buku.Penerbit = penerbit
+		buku.Pengarang = pengarang
+		buku.Tahun = tahun
+		buku.Stok = stok
+		buku.Harga = harga
+		buku.Gambar = gambar
+		return buku
+	}
+
+	//update buku
+	func (b buku) UpdateBuku (id int, judul string, penerbit string, pengarang string, tahun string, stok string, harga string, gambar string) models.Buku {
+		var buku models.Buku
+		buku.Id = id
+		buku.Judul = judul
+		buku.Penerbit = penerbit
+		buku.Pengarang = pengarang
+		buku.Tahun = tahun
+		buku.Stok = stok
+		buku.Harga = harga
+		buku.Gambar = gambar
+		return buku
+	}
+
+	//delete buku
+	func (b buku) DeleteBuku (id int) models.Buku {
+		var buku models.Buku
+		buku.Id = id
+		return buku
+	}
+
+	//search buku
+	func (b buku) SearchBuku (judul string) []models.Buku {
+		var bukus []models.Buku
+		db.Db.Select(&bukus, "SELECT * FROM buku WHERE judul LIKE ?", "%"+judul+"%")
+		return bukus
+	}
+
+
